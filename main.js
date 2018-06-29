ee66639150d87f1c438a6c240124373759f272c2
Pokemon-Xfallen <40690943+Pokemon-Xfallen@users.noreply.github.com>
2018-06-29 12:52:16 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const config = {
+    "TOKEN" : process.env.BOT_TOKEN,
+    "PREFIX" : "&"
+}
+
+const bot = new Discord.Client;
+
+bot.on("ready", () => {
+    console.log(`${bot.user.username} Is Onlien!!`);
+});
+
+bot.on("message", msg => {
+
+    if(msg.content === "ping"){
+        msg.reply("Pong")
+    }
+
+});
+
+bot.login(config.TOKEN)
+//bot.login(config.TOKEN2)
