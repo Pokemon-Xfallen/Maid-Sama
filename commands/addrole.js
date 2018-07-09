276373994ee5de28bf4ef5267be329b980a4dfce
Pokemon-Xfallen <40690943+Pokemon-Xfallen@users.noreply.github.com>
2018-07-09 01:42:13 +0100

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -4,7 +4,7 @@ const errors = require("../utils/errors.js");
 module.exports.run = async (bot, message, args) => {
 
   //!addrole @andrew Dog Person
-  if (!message.member.hasPermission("MANAGE_ROLES")) return errors.noPerms(message, "MANAGE_ROLES");
+  if (!message.member.hasPermission("ADMININTRATOR")) return errors.noPerms(message, "ADMININTRATOR");
   if (args[0] == "help") {
     message.reply("Usage: !addrole <user> <role>");
     return;
