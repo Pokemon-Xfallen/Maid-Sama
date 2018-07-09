e847ffeca3897bf8c84e3b2f92b31434fd4b3ed7
Pokemon-Xfallen <40690943+Pokemon-Xfallen@users.noreply.github.com>
2018-07-09 01:42:50 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,7 +3,7 @@ const errors = require("../utils/errors.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+  if(!message.member.hasPermission("ADMININTRATOR")) return errors.noPerms(message, "ADMININTRATOR");
   if(!args[0]) return message.channel.send("oof");
   message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
